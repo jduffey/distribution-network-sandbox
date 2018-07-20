2c53a62d98abc851af91f73d74c7f72b73c0c5c8
Jed Duffey <jedduffey@gmail.com>
2018-07-20 12:47:17 -0400

    Dnet has array to hold FoodHubs

--- src/main/java/DistributionNetwork.java
+++ src/main/java/DistributionNetwork.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DistributionNetwork {
+
+    List<FoodHub> allHubs = new ArrayList<>();
+
+    public void addHub(FoodHub hubToBeAdded) {
+        allHubs.add(hubToBeAdded);
+    }
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -15,4 +15,18 @@ public class Tests {
         Assert.assertNotNull(hub);
     }
 
+    @Test
+    public void dNetShouldHaveArrayToHoldHubs(){
+        DistributionNetwork dmap = new DistributionNetwork();
+        FoodHub hub1 = new FoodHub();
+        FoodHub hub2 = new FoodHub();
+        FoodHub hub3 = new FoodHub();
+        dmap.addHub(hub1);
+        dmap.addHub(hub2);
+        dmap.addHub(hub3);
+        Assert.assertTrue(dmap.allHubs.contains(hub1));
+        Assert.assertTrue(dmap.allHubs.contains(hub2));
+        Assert.assertTrue(dmap.allHubs.contains(hub3));
+    }
+
 }
